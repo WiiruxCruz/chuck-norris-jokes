93e01553b402ad00cf50241fcb9f7fa4ec56e2a9
WiiruxCruz <hialf.92@gmail.com>
2021-07-26 02:15:40 -0500

    Projecto inicial

---
+++ spring5-jokes-app-v2/src/test/java/mx/com/wiirux/spring5jokesappv2/Spring5JokesAppV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.com.wiirux.spring5jokesappv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5JokesAppV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
